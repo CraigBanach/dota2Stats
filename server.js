d03513c116a898ece59536f9bdda10bc0684c1ac
Craig Banach <craigsb93@gmail.com>
2016-05-14 20:57:40 +0000

    Added get request to Dota2 API on server launch.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 "use strict";
 
+var http = require("http");
 var express = require('express');
+require("dotenv").config();
 
 // Instantiate Express application
 var app = express();
@@ -8,7 +10,24 @@ var app = express();
 // serve static files from /views
 app.use(express.static(__dirname + '/views'));
 
+// Start server listening
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
+
+console.log(process.env.DOTA_2_KEY);
+
+http.get("http://api.steampowered.com/IDOTA2Match_205790/GetMatchHistory/V001/?key=" + process.env.DOTA_2_KEY, function(res) {
+    
+    var output = "";
+    
+    res.on("data", function(chunk) {
+        output += chunk;
+    });
+    
+    res.on("end", function() {
+        console.log(output);
+    });
+    
+})
